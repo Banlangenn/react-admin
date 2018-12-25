a73c906062ed8d19b7c0ad76607d94157fc4926a
banlangen <928012047@qq.com>
2018-12-26 00:51:35 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styles from './index.module.scss';
+import { Button, Slider } from 'antd';
+
 
 class App extends Component {
   render() {
+    console.log(styles)
+    console.log('=======')
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className={styles.bar}>hahahh</h1>
+          <div className={styles.bar}>
+            <Slider defaultValue={30}  />
+          </div>
           <p>
+            
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <Button type="danger">Danger</Button>
+          <Slider defaultValue={30}  />
+          <Slider range defaultValue={[20, 50]}  />
           <a
             className="App-link"
             href="https://reactjs.org"
