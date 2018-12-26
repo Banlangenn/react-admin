7c1f263d576212f1cffbd5ef8c774ff8b552d0a9
banlangen <928012047@qq.com>
2018-12-27 00:39:42 +0800

    调整

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,86 +1,9 @@
 
 import React, { Component } from 'react';
-import { Layout, Menu, SubMenu, Icon } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 import styles from './layout.module.scss'
-const { Header, Sider, Content } = Layout;
-
-{/* <Menu>
-  <Menu.Item>菜单项</Menu.Item>
-  <SubMenu title="子菜单">
-    <Menu.Item>子菜单项</Menu.Item>
-  </SubMenu>
-</Menu> */}
-
-const renderSubMenu =  ({ key, title, icon, link, sub, ...props }) => {
-   return  sub && sub.length>0 ? 
-   <SubMenu title={title} key={key || link}>
-       {renderMenu(sub)}
-   </SubMenu> 
-   :
-   <Menu.Item key="1">
-       <Icon type="user" />
-       <span>nav 1</span>
-   </Menu.Item>
-
-}
-
-const renderMenu =  (menus) => {
-    return menus.map(
-        item => renderSubMenu(item) 
-    )
-}
-// const renderMenu =  ({ menus, ...props }) => <Menu {...props}>
-//     {menus && menus.map(
-//         item => item.sub && item.sub.length ?
-//             renderSubMenu(item) : renderMenuItem(item)
-//     )}
-//     </Menu>;
-// const renderSubMenu =
-// ({ key, title, icon, link, sub, ...props }) =>
-//     <Menu.SubMenu
-//         key={key || link}
-//         title={
-//             <span>
-//                 {icon && <Icon type={icon} />}
-//                 <span className="nav-text">{title}</span>
-//             </span>
-//         }
-//         {...props}
-//     >
-//         {sub && sub.map(item => renderMenuItem(item))}
-//     </Menu.SubMenu>;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import Sider  from './Sider'
+const { Header, Content } = Layout;
 class CLayout extends Component {
   state = {
     collapsed: false,
@@ -97,27 +20,7 @@ class CLayout extends Component {
     return (
       <div className={ styles.layoutCustomTrigger }>
         <Layout  style={{ minHeight: '100vh' }}>
-        <Sider
-          trigger={null}
-          collapsible
-          collapsed={this.state.collapsed}
-        >
-          <div className={styles.logo} />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">
-              <Icon type="user" />
-              <span>nav 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="video-camera" />
-              <span>nav 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="upload" />
-              <span>nav 3</span>
-            </Menu.Item>
-          </Menu>
-        </Sider>
+        <Sider collapsed={this.state.collapsed} {...this.props}/>
         <Layout>
           <Header style={{ background: '#fff', padding: 0 }}>
             <Icon
