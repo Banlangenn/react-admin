f90437836d4a3faacc34930666c65caa9833b142
banlangen <928012047@qq.com>
2019-01-01 22:44:13 +0800

    s

--- src/layouts/Content/index.js
+++ src/layouts/Content/index.js
@@ -7,11 +7,17 @@ const { Content } = Layout;
 // import styles from './sider.module.scss'
 
 
-const HelloWorld = () => <h2>HelloWorld</h2>
+const HelloWorld = () =><div>
+    <h2>1. 怎么重定向子路由的第一个</h2>
+    <h2>2. 有些路径不是菜单，需要一个标识</h2>
+</div>
 const About = () => <h2>About  <Link to= "/test/two">/test/two</Link></h2>
 const ListComplete = () => <h2>ListComplete</h2>
 const UpdateLog = () => <h2>UpdateLog</h2>
-const PermissionMg = () => <h2>PermissionMg 侧边栏整体 依赖props-路由数据变化  <Link to= "/permission/about/about"> /permission/about/about</Link></h2>
+const PermissionMg = () => <div>
+    <h3>PermissionMg 侧边栏整体 依赖props-路由数据变化  <Link to= "/permission/about/about"> /permission/about/about</Link></h3>
+    <h2>1. 怎么重定向子路由的第一个</h2>
+    <h2>2. 有些路径不是菜单，需要一个标识不去渲染菜单</h2></div>
 const Index = () => <h2>Index</h2>
 const RolesMg = () => <h2>RolesMg  <Link to= "/test/two">/test/two</Link></h2>
 const json={
